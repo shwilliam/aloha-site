714446599930c2d5d5ea8831641d0fec6c609006
William L <w-lindvall@outlook.com>
2019-04-28 19:24:07 -0700

    fix links wo js

--- scripts/scroll-to.js
+++ scripts/scroll-to.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // query DOM for internal links
   document.getElementById('internal-links')
     .addEventListener('click', e => {
+      e.preventDefault() // prevent jump to internal anchor
       // click target is a button
       if (e.target && e.target.dataset && e.target.dataset.scrollTo) {
         document.querySelector(e.target.dataset.scrollTo)
